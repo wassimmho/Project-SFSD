ade4ddab6840cb63ce39ea5d371da0b19fb834af
wassimmho <wassim.mho000@gamil.com>
2024-12-25 00:08:27 -1200

    phushing

---
+++ build/build.bat
@@ -0,0 +1,3 @@
+gcc -o debug\myprogram Project-SFSD/version1.c  -Iinclude -LWlib -lraylib -lopengl32 -lgdi32 -lwinmm && .\debug\myprogram
+
+
---
+++ build/build.sh
@@ -0,0 +1,2 @@
+gcc   -o debug/myprogram ./src/main.c ./src/abstract.c ./src/index.c -Iinclude -Llib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 &&  ./debug/myprogram
+    
